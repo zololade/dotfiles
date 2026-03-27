8240d0dc35560a6ef8811af1eebbd7c5b654ef25
Ololade <73138104+zololade@users.noreply.github.com>
2026-03-27 14:36:25 +0100

    change wall paper directory

--- setup.js
+++ setup.js
@@ -11,25 +11,45 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const CONFIG = path.join(HOME, ".config");
+const LOCAL = path.join(HOME, ".local", "bin");
+fs.mkdirSync(LOCAL, { recursive: true });
+
 const SYMLINK_FOLDERS = [
-  { source: "btop", destination: "btop" },
-  { source: "hyprland", destination: "hypr" },
-  { source: "kitty", destination: "kitty" },
-  { source: "rofi", destination: "rofi" },
-  { source: "swaync", destination: "swaync" },
-  { source: "waybar", destination: "waybar" },
-  { source: "wlogout", destination: "wlogout" },
-  { source: "starship.toml", destination: "starship.toml" },
-  { source: "coolwall.js", destination: "coolwall.js" },
-  { source: "gtk-3.0", destination: "gtk-3.0" },
-  { source: "gtk-4.0", destination: "gtk-4.0" },
-  { source: "mimeapps.list", destination: "mimeapps.list" },
-  { source: "nvim", destination: "nvim" },
+  { source: "btop", destination: "btop", home: "config" },
+  { source: "hyprland", destination: "hypr", home: "config" },
+  { source: "kitty", destination: "kitty", home: "config" },
+  { source: "rofi", destination: "rofi", home: "config" },
+  { source: "swaync", destination: "swaync", home: "config" },
+  { source: "waybar", destination: "waybar", home: "config" },
+  { source: "wlogout", destination: "wlogout", home: "config" },
+  {
+    source: "starship.toml",
+    destination: "starship.toml",
+    home: "config",
+  },
+  {
+    source: "coolwall.js",
+    destination: "coolwall.js",
+    home: "local",
+  },
+  { source: "gtk-3.0", destination: "gtk-3.0", home: "config" },
+  { source: "gtk-4.0", destination: "gtk-4.0", home: "config" },
+  {
+    source: "mimeapps.list",
+    destination: "mimeapps.list",
+    home: "config",
+  },
+  { source: "nvim", destination: "nvim", home: "config" },
 ];
 
-function createSymlink({ source, destination }) {
+const LOCATIONS = {
+  config: CONFIG,
+  local: LOCAL,
+};
+
+function createSymlink({ source, destination, home }) {
   const currentPath = path.join(__dirname, "config", source);
-  const symlinkPath = path.join(CONFIG, destination);
+  const symlinkPath = path.join(LOCATIONS[home], destination);
 
   if (!fs.existsSync(currentPath)) {
     console.log(`Missing source: ${source}`);
